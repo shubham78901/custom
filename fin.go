84c0e2d3b062fa34d3252634611d12fa3eefd367
Shubham Gautam <70124011+shubham78901@users.noreply.github.com>
2023-07-14 12:08:52 +0530

    sdxsx.

--- fin.go
+++ fin.go
@@ -6,7 +6,6 @@ import (
 	"log"
 
 	"github.com/libsv/go-bk/wif"
-	"github.com/libsv/go-bt/bscript"
 	"github.com/libsv/go-bt/v2"
 	"github.com/libsv/go-bt/v2/sighash"
 	"github.com/libsv/go-bt/v2/unlocker"
@@ -18,8 +17,10 @@ func main() {
 
 func WriteDataToBlockchain() {
 	tx := bt.NewTx()
-	// s := &bscript.Script{}
-	// // _ = s.AppendOpCode()
+	// unlocking, err := bscript.NewFromASM("76a91493e3ac50804356fb8ed57cb911d8567740a2773888ac")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	err := tx.From(
 		"ca32b83f178c2aef51ca54a8a89f560091618161a43d489d2e239af77771e655",
@@ -31,15 +32,15 @@ func WriteDataToBlockchain() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	tx.AddOutput(&bt.Output{
-		Satoshis:      67,
-		LockingScript: &bscript.Script{},
-	})
+	//defined in library
+	err = tx.AddHashPuzzleOutput("shubham", "76a91493e3ac50804356fb8ed57cb911d8567740a2773888ac", 600)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	decodedWif, _ := wif.DecodeWIF("L1Dgi5VxvdFuYiWrK3v8T51Wuu4t9Kjkn3qCs4XNkNEDhuDvUCG4")
 
-	err := tx.FillInput(context.Background(), &unlocker.Simple{PrivateKey: decodedWif.PrivKey},
+	err = tx.FillInput(context.Background(), &unlocker.Simple{PrivateKey: decodedWif.PrivKey},
 		bt.UnlockerParams{uint32(0), sighash.AllForkID},
 	)
 
